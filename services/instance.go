82defa3119ee73d209d84c86b4bdf2fd725345cb
Rafael George <george.rafael@gmail.com>
2016-03-29 09:32:44 -0400

    Add comments to new public method

--- services/instance.go
+++ services/instance.go
@@ -31,6 +31,7 @@ func (is *InstanceService) Show(playbookID, ID string) (broadway.Instance, error
 	return instance, nil
 }
 
+// AllWithPlaybookID returns all the instances for an specified playbook id
 func (is *InstanceService) AllWithPlaybookID(playbookID string) ([]broadway.Instance, error) {
 	return is.repo.FindByPlaybookID(playbookID)
 }
