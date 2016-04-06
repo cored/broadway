81d23ff0d0d8999cb9a136d24a03e391cdfe659a
therealplato <therealplato@gmail.com>
2016-04-06 13:08:14 -0400

    Add first test and implementation for SlackCommand

---
+++ services/slack_command_handler.go
@@ -0,0 +1,68 @@
+package services
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/golang/glog"
+)
+
+// SlackCommand represents a user command that came in from Slack
+type SlackCommand interface {
+	Execute() error
+}
+
+type dummyCommand struct {
+	args []string
+}
+
+func (c *dummyCommand) Execute() error {
+	return errors.New("foo")
+}
+
+type setvarCommand struct {
+	args []string
+	Vars map[string]string
+	is   *InstanceService
+}
+
+func (c *setvarCommand) Execute() error {
+	c.Vars = make(map[string]string)
+	kvs := c.args[3:len(c.args)] // from e.g. "setvar foo bar var1=val1 var2=val2"
+	for _, kv := range kvs {
+		kv = strings.TrimPrefix(kv, "=")
+		kv = strings.TrimSuffix(kv, "=")
+		tmp := strings.Split(kv, "=")
+		if len(tmp) != 2 {
+			glog.Warning("Setvar tried to parse badly formatted variable: " + kv)
+			return errors.New("This is not the proper syntax. ex: var1=val1")
+		}
+		c.Vars[tmp[0]] = tmp[1]
+	}
+	i, err := c.is.Show(c.args[1], c.args[2])
+	if err != nil {
+		glog.Warningf("Cannot setvars for not found instance %s/%s\n", c.args[1], c.args[2])
+		return errors.New("Instance not found")
+	}
+	i.Vars = c.Vars
+	err = c.is.repo.Save(i)
+	if err != nil {
+		glog.Errorf("Failed to save instance %s/%s with new vars\n", c.args[1], c.args[2])
+		return errors.New("Instance not updated")
+	}
+	return nil
+}
+
+// BuildSlackCommand takes a string and some context and creates a SlackCommand
+func BuildSlackCommand(payload string, is *InstanceService) SlackCommand {
+	terms := strings.Split(payload, " ")
+	switch terms[0] {
+	case "dummy":
+		return &dummyCommand{args: terms}
+	case "setvar": // setvar foo bar var1=val1 var2=val2
+		return &setvarCommand{args: terms, is: is}
+		// case "help"
+	default:
+		return &dummyCommand{args: terms}
+	}
+}
