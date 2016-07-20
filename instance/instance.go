716b580176b39498ca5549b8aa83d9c9416bb0f8
Rafael George <george.rafael@gmail.com>
2016-07-20 08:18:31 -0400

    Initial refactor to reduce the indirection for retrieving data
    - Remove instance.Repository
    - Move store.etcd to it's own package
    - Add instance.Path to centralize the creation of a Path for an instance

--- instance/instance.go
+++ instance/instance.go
@@ -2,9 +2,37 @@ package instance
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 
-	"github.com/namely/broadway/env"
+	"github.com/namely/broadway/store"
 )
+
+// NotFoundError instance not found error
+type NotFoundError string
+
+func (e NotFoundError) Error() string {
+	return fmt.Sprintf("Instance with path: %s was not found", string(e))
+}
+
+// ErrMalformedSaveData in case an instance cannot be marshall
+var ErrMalformedSaveData = errors.New("Saved data for this instance is malformed")
+
+// Path represents a path for an instance
+type Path struct {
+	rootPath   string
+	playbookID string
+	id         string
+}
+
+// NewPath constructor for a path
+func NewPath(rootPath, playbookID, id string) Path {
+	return Path{rootPath, playbookID, id}
+}
+
+func (p Path) String() string {
+	return fmt.Sprintf("%s/instances/%s/%s", p.rootPath, p.playbookID, p.id)
+}
 
 // Instance entity
 type Instance struct {
@@ -41,6 +69,21 @@ func (i *Instance) JSON() (string, error) {
 }
 
 // Path for an instance
-func (i *Instance) Path() string {
-	return env.EtcdPath + "/instances/" + i.PlaybookID + "/" + i.ID
+// func (i *Instance) Path() string {
+// 	return env.EtcdPath + "/instances/" + i.PlaybookID + "/" + i.ID
+// }
+
+// FindByPath find an instance based on it's path
+func FindByPath(store store.Store, path Path) (*Instance, error) {
+	var instance Instance
+
+	i := store.Value(path.String())
+	if i == "" {
+		return nil, NotFoundError(path.String())
+	}
+	err := json.Unmarshal([]byte(i), &instance)
+	if err != nil {
+		return nil, ErrMalformedSaveData
+	}
+	return &instance, nil
 }
---
+++ instance/instance_test.go
@@ -0,0 +1,60 @@
+package instance_test
+
+import (
+	"testing"
+
+	. "github.com/namely/broadway/instance"
+	"github.com/namely/broadway/store"
+	"github.com/stretchr/testify/assert"
+)
+
+type FakeStore struct {
+	MockSetValue func(path, value string) error
+	MockValue    func(path string) string
+	MockValues   func(path string) map[string]string
+	MockDelete   func(path string) error
+}
+
+func (fs *FakeStore) SetValue(path, value string) error {
+	return fs.MockSetValue(path, value)
+}
+
+func (fs *FakeStore) Value(path string) string {
+	return fs.MockValue(path)
+}
+
+func (fs *FakeStore) Values(path string) map[string]string {
+	return fs.MockValues(path)
+}
+
+func (fs *FakeStore) Delete(path string) error {
+	return fs.MockDelete(path)
+}
+
+func TestFindByPath(t *testing.T) {
+	testcases := []struct {
+		Scenario           string
+		Path               Path
+		Store              store.Store
+		ExpectedPlaybookID string
+		ExpectedError      error
+	}{
+		{
+			Scenario: "When the instance is properly save",
+			Path:     NewPath("etcdPath", "test", "id"),
+			Store: &FakeStore{
+				MockValue: func(path string) string {
+					return `{"playbook_id":"test", "id": "id", "status": "deployed"}`
+				},
+			},
+			ExpectedPlaybookID: "test",
+			ExpectedError:      nil,
+		},
+	}
+
+	for _, tc := range testcases {
+		returnedInstance, err := FindByPath(tc.Store, tc.Path)
+		assert.Equal(t, tc.ExpectedError, err, tc.Scenario)
+		assert.Equal(t, tc.ExpectedPlaybookID, returnedInstance.PlaybookID)
+	}
+}
