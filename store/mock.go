075da421e9da1bb3acdec3ece84633a2dd7cd78a
Rafael George <george.rafael@gmail.com>
2016-07-22 08:50:24 -0400

    Add comments for store.FakeStore

--- store/mock.go
+++ store/mock.go
@@ -1,5 +1,6 @@
 package store
 
+// FakeStore mock for the store
 type FakeStore struct {
 	MockSetValue func(path, value string) error
 	MockValue    func(path string) string
@@ -7,18 +8,22 @@ type FakeStore struct {
 	MockDelete   func(path string) error
 }
 
+// SetValue mocked implementation
 func (fs *FakeStore) SetValue(path, value string) error {
 	return fs.MockSetValue(path, value)
 }
 
+// Value mocked implementation
 func (fs *FakeStore) Value(path string) string {
 	return fs.MockValue(path)
 }
 
+// Values mocked implementation
 func (fs *FakeStore) Values(path string) map[string]string {
 	return fs.MockValues(path)
 }
 
+// Delete mocked implementation
 func (fs *FakeStore) Delete(path string) error {
 	return fs.MockDelete(path)
 }
